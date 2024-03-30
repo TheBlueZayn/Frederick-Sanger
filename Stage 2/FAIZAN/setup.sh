9e4367707b461eff8661c17269e0dcf29db6c8a0
MUHAMADFAIZANKHAN <162900134+MUHAMADFAIZANKHAN@users.noreply.github.com>
2024-03-30 21:34:15 +0500

    Add files via upload

---
+++ Stage 2/FAIZAN/setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+
+# Install FastQC for quality control
+sudo apt install fastqc
+
+# Install MultiQC
+sudo apt install multiqc
+
+# Install Fastp
+sudo apt install fastp
+
+# Install bwa for genome mapping
+sudo apt install bwa
+
+# Install samtools for working with SAM/BAM files
+sudo apt install samtools
+
+# Install bcftools for working with vcf
+sudo apt install bcftools
