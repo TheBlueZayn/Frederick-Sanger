a00bd85828dc2586052a4048fd94cf11ac1993d1
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2024-03-30 13:28:45 +0100

    Update script.sh

--- Stage 2/Zaynab/script.sh
+++ Stage 2/Zaynab/script.sh
@@ -15,7 +15,7 @@ fastqc data/fastq/*.fastq.gz -o qc_report
 
 multiqc qc_report/*_fastqc.zip -o qc_report
 
-fastp 
+fastp --detect_adapter_for_pe --overrepresentation_analysis --correction --cut_right --html trimmed/ERR8774458.fastp.html --json --thread 2 -i data/fastq/ERR8774458_1.fastq.gz -I data/fastq/ERR8774458_2.fastq.gz -o trimmed/ERR8774458_1.fastq.gz -O trimmed/ERR8774458_2.fastq.gz
 
 
 
