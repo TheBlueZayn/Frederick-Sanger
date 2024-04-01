69a0243cfc086ec44d3f06b8e726138ad3571508
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2024-04-01 14:10:40 +0100

    edits

--- Stage 2/Zaynab/script.sh
+++ Stage 2/Zaynab/script.sh
@@ -1,21 +1,46 @@
 #!bin/bash
 
+# Create directory for variant calling
 mkdir variant_calling && cd variant_calling
 
-mkdir data
-mkdir data/fastq
-mkdir data/ref
+# Create directory for data
+mkdir -p data/fastq data/ref
 
-wget -P data/fastq {$} {$}
+# Download reads and reference genome
+wget -P data/fastq https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
+wget -P data/ref https://zenodo.org/records/10886725/files/Reference.fasta
 
-wget -P data/ref {$}
-
+# Perform quality check on reads
 mkdir qc_report
 fastqc data/fastq/*.fastq.gz -o qc_report
 
+# Aggregate fastqc reports
 multiqc qc_report/*_fastqc.zip -o qc_report
 
+# Trim faulty reads with fastp
+mkdir trimmed 
 fastp --detect_adapter_for_pe --overrepresentation_analysis --correction --cut_right --html trimmed/ERR8774458.fastp.html --json --thread 2 -i data/fastq/ERR8774458_1.fastq.gz -I data/fastq/ERR8774458_2.fastq.gz -o trimmed/ERR8774458_1.fastq.gz -O trimmed/ERR8774458_2.fastq.gz
 
+# Map reads to reference genome
+mkdir -p results/sam results/bam results/bcf results/vcf
+bwa index data/ref/Reference.fasta
+bwa mem data/ref/Reference.fasta trimmed/ERR8774458_1.fastq.gz trimmed/ERR8774458_2.fastq.gz > results/sam/ERR8774458.aligned.sam
 
 
+samtools view -b -F 0xc results/bam/ERR8774458.aligned.bam -o results/bam/ERR8774458.aligned.filtered.bam
+samtools sort -@ 8 -n results/bam/ERR8774458.aligned.filtered.bam -o results/bam/ERR8774458.aligned.sorted.n.bam
+samtools fixmate -m results/bam/ERR8774458.aligned.sorted.n.bam results/bam/ERR8774458.aligned.fixmate.bam
+
+samtools sort -@ 8 results/bam/ERR8774458.aligned.fixmate.bam -o results/bam/ERR8774458.aligned.sorted.p.bam
+samtools markdup -r -@ 8 results/bam/ERR8774458.aligned.sorted.p.bam results/bam/ERR8774458.aligned.dedup.bam
+
+freebayes -f data/ref/Reference.fasta -b results/bam/ERR8774458.aligned.dedup.bam --vcf results/vcf/ERR8774458.vcf
+bgzip results/vcf/ERR8774458.vcf
+
+bcftools index results/vcf/ERR8774458.vcf.gz
+
+# Counting all variants
+zgrep -v -c "^#" results/vcf/ERR8774458.vcf.gz
+
+bcftools view -v snps results/vcf/ERR8774458.vcf.gz|grep -v -c "^#"
+bcftools view -v snps results/vcf/ERR8774458.vcf.gz -Oz -o results/vcf/snps.vcf.gz
