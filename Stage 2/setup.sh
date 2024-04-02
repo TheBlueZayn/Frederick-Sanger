5b09ea6864a24bb033bfcdba8bd146c6f17fa022
yousrawali <162142882+yousrawali@users.noreply.github.com>
2024-04-02 11:20:24 -0700

    Add files via upload
    Yousra wali

---
+++ Stage 2/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install fastqc for quality assessment
+sudo apt-get install fastqc
+
+# install multiqc to aggregate reports
+sudo apt-get install multiqc
+
+#install fastp for triming
+sudo apt-get install fastp
+
+#install Bwa for alignment
+sudo apt-get install bwa
+
+#install samtools for file conversions
+sudo apt-get install samtools
+
+#install freebayes for variant calling 
+sudo apt-get install freebayes
+
+#install bcftools for analyzing vcf files
+sudo apt-get install bcftools 
