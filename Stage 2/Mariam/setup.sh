749f1f38b6a1bdf7ff203643a198bd43a065d24e
Mariamkhaled15 <163095154+Mariamkhaled15@users.noreply.github.com>
2024-04-02 06:44:18 +0200

    Add files via upload

---
+++ Stage 2/Mariam/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get intsall bwa 
+sudo apt-get install fastqc
+sudo apt-get install fastp
+sudo apt-get install multiqc
+sudo apt-get install samtools
+sudo apt-get install bcftools
+
