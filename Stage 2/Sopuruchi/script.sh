38833635ddfd042549aeeb9f09093fc562ab721d
Sopuruchilizbeth <164163150+Sopuruchilizbeth@users.noreply.github.com>
2024-04-02 05:30:37 +0100

    Update script.sh

--- Stage 2/Sopuruchi/script.sh
+++ Stage 2/Sopuruchi/script.sh
@@ -25,4 +25,20 @@ mkdir trimmed
 # Trim fastq data and output in the trimmed folder
 fastp -i data/fastq/ERR8774458_1.fastq.gz -I data/fastq/ERR8774458_2.fastq.gz -o trimmed/trim_1.fastq.gz -O trimmed/trim_2.fastq.gz
 
-# 
+# Index the reference data
+bwa index -a bwtsw data/ref/Reference.fasta data/ref
+
+# Align the reference data and the trimmed fastq data to produce a sam file
+bwa mem data/ref/Reference.fasta trimmed/trim_1.fastq.gz trimmed/trim_2.fastq.gz > aligned.sam
+
+# Convert the sam file into a bam file for sorting and indexing 
+samtools view -hbo aligned.bam aligned.sam
+
+# Sort the bam file
+samtools sort aligned.bam -o aligned.sorted.bam
+
+# Index sorted bam file
+samtools index aligned.sorted.bam
+
+# Call variants from the sorted bam file
+bcftools mpileup -Ou -f data/ref/Reference.fasta aligned.sorted.bam |bcftools call -Ov -mv > aligned.vcf  
