3eec498d382fdee2d88fb3f22d2833f50aafbbab
yousrawali <162142882+yousrawali@users.noreply.github.com>
2024-04-04 04:18:36 -0700

    Update Script.sh for any input data

--- Stage 2/Yousra/Script.sh
+++ Stage 2/Yousra/Script.sh
@@ -1,89 +1,54 @@
 #!/bin/bash
 
-1. # make a directory for your pipeline
- mkdir variant-calling && cd variant-calling
-
-2. # make subdirectory for your datasets
- mkdir data
-mkdir data/fastq
-mkdir data/ref
-cd data/fastq
-
-3. # once in your data/fastq directory download your data
-cd data/fastq
-wget https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz
-wget https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
-
-4. # go to  data/ref directory and download reference data
-cd ..
-cd ref
-wget https://zenodo.org/records/10886725/files/Reference.fasta
-
-5. # download and run fastqc on the forward and reverse strands
-
-sudo apt-get install fastqc
-fastqc ERR8774458_1.fastq.gz ERR8774458_2.fastq.gz
-
-6. # download and run multiqc to aggregate data
-
-sudo apt-get install multiqc
-multiqc ERR8774458_1_fastqc.zip ERR8774458_2_fastqc.zip
-
-7. # download fastp and trim your reads
-
-sudo apt-get install fastp
-
-fastp -i ERR8774458_1.fastq.gz -I ERR8774458_2.fastq.gz -o output_R1_trimmed.fastq.gz -O output_R2_trimmed.fastq.gz --detect_adapter_for_pe  --trim_poly_g --html report.html --json report.json
-
-8. #install and use bwa for indexing data and mapping
-
-sudo apt-get install bwa
-
-bwa index Reference.fasta
+#  demand user input
+read -p "Input name of the gene:" name
+read -p "Input url to forward read:" R1
+read -p "Input url to backward read:" R2
+read -p "Input url to reference fasta data:" ref
 
 
-bwa mem data/ref/Reference.fasta data/fastq/output_R1_trimmed.fastq.gz data/fastq/output_R2_trimmed.fastq.gz > aligned_reads.sam
+# create the required directories to store your data
 
-8. # convert sam to bam file
+mkdir -p $name/data $name/Qc_reports $name/trimmed_results $name/Results
 
-9. # install samtools and use it to convert sam files to bam files and sort reads
+# download data sets
+wget -P $name/data $R1
+wget -P $name/data $R2
+wget -P $name/data $ref
 
-sudo apt-get install samtools
+# quality control
 
-samtools flagstat aligned_reads.sam
+fastqc $name/data/*.fastq.gz -o $name/Qc_reports
 
-samtools view -bS aligned_reads.sam | samtools sort -o sorted_reads.bam
+# Multiqc to aggregate reports
 
-10. # remove duplicates and index using samtools
+multiqc $name/Qc_reports/*.fastqc.zip -o $name/Qc_reports
 
+# perform trimming using fastp and save output in the trimmed_results directory
+fastp -i $name/data/"$name"*1.fastq.gz -I $name/data/"$name"*2.fastq.gz -o $name/trimmed_results/"$name"_R1.trimmed.fastq.gz -O $name/trimmed_results/"$name"_R2.trimmed.fastq.gz --html $name/trimmed_results/"$name"_fastp.html --json $name/trimmed_results/"$name"_fastp.json
 
-samtools rmdup sorted_reads.bam deduplicated_reads.bam
+# perform indexing using bwa
+bwa index $name/data/reference.fasta
 
-samtools index deduplicated_reads.bam
+#perform read alignments
+bwa mem $name/data/reference.fasta $name/trimmed_results/"$name"_R1.trimmed.fastq.gz $name/trimmed_results/"$name"_R2.trimmed.fastq.gz > $name/Results/"$name".sam
 
-11 # install freebayes/bcftools and call variants
+# use samtools to convert sam files to bam
+samtools view -S -b $name/Results/"$name".sam > $name/Results/"$name".bam
 
-sudo apt-get install freebayes
+# perform sorting of bam files
+samtools sort -o $name/Results/"$name".sorted.bam $name/Results/"$name".bam
 
-freebayes -f Reference.fasta ~/variant-calling/deduplicated_reads.bam > ~/variant-calling/variants.vcf
+# perform indexing of bam files
 
-12. # install tabix to zip vcf file and bcftools for analysis of the called variants
+samtools index $name/Results/"$name".sorted.bam
 
-sudo apt-get install bcftools
-sudo apt-get install tabix
+# use bcftools to pileup format 
+bcftools mpileup -O b -o $name/Results/"$name".bcf -f $name/data/reference.fasta $name/Results/"$name".sorted.bam
 
-bgzip variants.vcf
+# Call variants using bcftool and create vcf file
+bcftools call -m -v -o $name/Results/"$name".variants.vcf $name/Results/"$name".bcf
 
-bcftools index variants.vcf.gz
-
-
-12. # get the total no of variants, total no of snps and indels respectively 
-
-zgrep -v -c '^#' variants.vcf.gz 
-
-bcftools view -v snps variants.vcf.gz | grep -v -c '^#
-
-bcftools view -v indels VCF/Result.vcf.gz | grep -v -c '^#'
 
 
 
