c7732dbd44321d4c200508810a04d818becf2802
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2024-04-04 09:06:34 +0100

    Update script.sh

--- Stage 2/Zaynab/script.sh
+++ Stage 2/Zaynab/script.sh
@@ -8,9 +8,9 @@ read -p "Input link to reference fasta data:" reference
 mkdir -p $name/data/ref $name/qc_report $name/trimmed $name/results 
 
 # Download datasets and store in data folder
-wget -O "$name"_R1.fastq.gz -P $name/data $R1
-wget -O "$name"_R2.fastq.gz -P $name/data $R2
-wget -O reference.fasta -P $name/data/ref $reference
+wget -P $name/data $R1 -O "$name"_R1.fastq.gz
+wget -P $name/data $R2 -O "$name"_R2.fastq.gz
+wget -P $name/data/ref $reference -O reference.fasta
 
 
 # Perform quality control check on both reads, output to qc_folder folder
