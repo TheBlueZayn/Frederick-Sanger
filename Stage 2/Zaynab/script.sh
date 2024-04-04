64ae2e1a2a5ee55165d258aac0488e4d09deeb14
TheBlueZayn <106994375+TheBlueZayn@users.noreply.github.com>
2024-04-04 09:12:53 +0100

    Update script.sh

--- Stage 2/Zaynab/script.sh
+++ Stage 2/Zaynab/script.sh
@@ -8,42 +8,42 @@ read -p "Input link to reference fasta data:" reference
 mkdir -p $name/data/ref $name/qc_report $name/trimmed $name/results 
 
 # Download datasets and store in data folder
-wget -P $name/data $R1 -O "$name"_R1.fastq.gz
-wget -P $name/data $R2 -O "$name"_R2.fastq.gz
-wget -P $name/data/ref $reference -O reference.fasta
+wget -P $name/data -O "$name"_R1.fastq.gz $R1
+wget -P $name/data -O "$name"_R2.fastq.gz $R2
+wget -P $name/data/ref -O reference.fasta $reference
 
 
-# Perform quality control check on both reads, output to qc_folder folder
-fastqc $name/data/*.fastq.gz -o $name/qc_report
+# # Perform quality control check on both reads, output to qc_folder folder
+# fastqc $name/data/*.fastq.gz -o $name/qc_report
 
-# Aggregate fastqc reports 
-multiqc $name/qc_report/*_fastqc.zip -o $name/qc_report
+# # Aggregate fastqc reports 
+# multiqc $name/qc_report/*_fastqc.zip -o $name/qc_report
 
-# Trim faulty reads with fastp, output to trimmed folder
-fastp -i $name/data/"$name"*1.fastq.gz -I $name/data/"$name"*2.fastq.gz -o $name/trimmed/"$name"_R1.trimm.fastq.gz -O $name/trimmed/"$name"_R2.trimm.fastq.gz --html $name/trimmed/"$name"_fastp.html --json $name/trimmed/"$name"_fastp.json
+# # Trim faulty reads with fastp, output to trimmed folder
+# fastp -i $name/data/"$name"*1.fastq.gz -I $name/data/"$name"*2.fastq.gz -o $name/trimmed/"$name"_R1.trimm.fastq.gz -O $name/trimmed/"$name"_R2.trimm.fastq.gz --html $name/trimmed/"$name"_fastp.html --json $name/trimmed/"$name"_fastp.json
 
-# Map reads to reference genome with bwa
-bwa index $name/data/ref/reference.fasta
+# # Map reads to reference genome with bwa
+# bwa index $name/data/ref/reference.fasta
 
-# Align reads to reference genome
-bwa mem $name/data/ref/reference.fasta $name/trimmed/"$name"_R1.trimm.fastq.gz $name/trimmed/"$name"_R2.trimm.fastq.gz > $name/results/"$name".aligned.sam
+# # Align reads to reference genome
+# bwa mem $name/data/ref/reference.fasta $name/trimmed/"$name"_R1.trimm.fastq.gz $name/trimmed/"$name"_R2.trimm.fastq.gz > $name/results/"$name".aligned.sam
 
-# Convert sam file to bam file
-samtools view -S -b $name/results/"$name".aligned.sam > $name/results/"$name".aligned.bam
+# # Convert sam file to bam file
+# samtools view -S -b $name/results/"$name".aligned.sam > $name/results/"$name".aligned.bam
 
-# Sort sequemce
-samtools sort -o $name/results/"$name".aligned.sorted.bam $name/results/"$name".aligned.bam
+# # Sort sequemce
+# samtools sort -o $name/results/"$name".aligned.sorted.bam $name/results/"$name".aligned.bam
 
-# Index sorted bam file with samtools
-samtools index $name/results/"$name".aligned.sorted.bam
+# # Index sorted bam file with samtools
+# samtools index $name/results/"$name".aligned.sorted.bam
 
-# Generate pileup format for bam file
-bcftools mpileup -O b -o $name/results/"$name".bcf -f $name/data/ref/reference.fasta $name/results/"$name".aligned.sorted.bam
+# # Generate pileup format for bam file
+# bcftools mpileup -O b -o $name/results/"$name".bcf -f $name/data/ref/reference.fasta $name/results/"$name".aligned.sorted.bam
 
-# Identify variants using bcftools call and generately variant (vcf) file
-bcftools call -m -v -o $name/results/"$name".variants.vcf $name/results/"$name".bcf
+# # Identify variants using bcftools call and generately variant (vcf) file
+# bcftools call -m -v -o $name/results/"$name".variants.vcf $name/results/"$name".bcf
 
-# Compress variant file
-# bgzip $name/results/"$name".variants.vcf
+# # Compress variant file
+# # bgzip $name/results/"$name".variants.vcf
 
 
