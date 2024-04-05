7032e9f694e8a8457db101b61e48e905727df050
yousrawali <162142882+yousrawali@users.noreply.github.com>
2024-04-05 04:41:08 -0700

    Update Script.sh

--- Stage 2/Yousra/Script.sh
+++ Stage 2/Yousra/Script.sh
@@ -1,55 +1,85 @@
 #!/bin/bash
 
-#  demand user input
-read -p "Input name of the gene:" name
-read -p "Input url to forward read:" R1
-read -p "Input url to backward read:" R2
-read -p "Input url to reference fasta data:" ref
+# Define reference genome URL
+reference_url="https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/raw_reads/reference.fasta"
+
+# Define a list of datasets URLs and corresponding reverse strands (paired-end sequencing)
+# Each dataset should have a corresponding reverse strand file
+datasets=("https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R1.fastq.gz"
+          "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R1.fastq.gz"
+          "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R1.fastq.gz"
+          "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R1.fastq.gz"
+          "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R1.fastq.gz")
+
+reverse_datasets=("https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R2.fastq.gz"
+                   "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R2.fastq.gz"
+                   "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R2.fastq.gz"
+                   "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R2.fastq.gz"
+                   "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R2.fastq.gz")
+
+# Directory to store downloaded datasets, FastQC results, MultiQC results, and output VCF files
+dataset_dir="datasets"
+fastqc_dir="fastqc_results"
+multiqc_dir="multiqc_results"
+vcf_dir="vcf_files"
+
+# Create the directories if they don't exist
+mkdir -p "$dataset_dir" "$fastqc_dir" "$multiqc_dir" "$vcf_dir"
+
+# Download reference genome
+wget -P "$dataset_dir" "$reference_url"
+
+# Extract reference genome filename
+reference_file="${reference_url##*/}"
+
+# Build BWA index
+bwa index "$dataset_dir/$reference_file"
+
+# Perform quality control using FastQC for each dataset
+for fastq_file in "${datasets[@]}"; do
+    fastq_basename=$(basename "$fastq_file")
+    fastqc "$fastq_file" -o "$fastqc_dir"
+done
+
+# Generate a MultiQC report for all FastQC results
+multiqc "$fastqc_dir" -o "$multiqc_dir"
+
+# Iterate over each dataset URL
+for i in "${!datasets[@]}"; do
+    # Download forward and reverse strand datasets
+    forward_file="${datasets[i]}"
+    reverse_file="${reverse_datasets[i]}"
+    wget -P "$dataset_dir" "$forward_file" "$reverse_file"
+
+    # Step 1: Perform read alignment using BWA for forward reads
+    forward_alignment_output="${forward_file##*/}.sam"
+  bwa mem "$dataset_dir/$reference_file" "$dataset_dir/${forward_file##*/}" > "$dataset_dir/$forward_alignment_output"
+
+    # Step 2: Perform read alignment using BWA for reverse reads
+    reverse_alignment_output="${reverse_file##*/}.sam"
+    bwa mem "$dataset_dir/$reference_file" "$dataset_dir/${reverse_file##*/}" > "$dataset_dir/$reverse_alignment_output"
+
+    # Step 3: Merge alignments of forward and reverse reads
+    merged_alignment_output="${forward_file##*/}_merged.bam"
+    samtools merge -f "$dataset_dir/$merged_alignment_output" "$dataset_dir/$forward_alignment_output" "$dataset_dir/$reverse_alignment_output"
+
+    # Step 4: Sort and index the merged alignment file
+    sorted_alignment_output="${merged_alignment_output%.bam}_sorted.bam"
+    samtools sort -o "$dataset_dir/$sorted_alignment_output" "$dataset_dir/$merged_alignment_output"
+    samtools index "$dataset_dir/$sorted_alignment_output"
+ 
+    # Step 5: Variant calling using samtools mpileup and bcftools call
+    variant_output="${sorted_alignment_output%.bam}.vcf"
+    samtools mpileup -uf "$dataset_dir/$reference_file" "$dataset_dir/$sorted_alignment_output" | bcftools call -mv > "$vcf_dir/$variant_output"
+
+    echo "Variants for ${forward_file##*/} and ${reverse_file##*/} are in $vcf_dir/$variant_output"
+
+echo "variants called successfully"
+done
 
 
-# create the required directories to store your data
 
-mkdir -p $name/data $name/Qc_reports $name/trimmed_results $name/Results
 
-# download data sets
-wget -P $name/data $R1
-wget -P $name/data $R2
-wget -P $name/data $ref
-
-# quality control
-
-fastqc $name/data/*.fastq.gz -o $name/Qc_reports
-
-# Multiqc to aggregate reports
-
-multiqc $name/Qc_reports/*.fastqc.zip -o $name/Qc_reports
-
-# perform trimming using fastp and save output in the trimmed_results directory
-fastp -i $name/data/"$name"*1.fastq.gz -I $name/data/"$name"*2.fastq.gz -o $name/trimmed_results/"$name"_R1.trimmed.fastq.gz -O $name/trimmed_results/"$name"_R2.trimmed.fastq.gz --html $name/trimmed_results/"$name"_fastp.html --json $name/trimmed_results/"$name"_fastp.json
-
-# perform indexing using bwa
-bwa index $name/data/reference.fasta
-
-#perform read alignments
-bwa mem $name/data/reference.fasta $name/trimmed_results/"$name"_R1.trimmed.fastq.gz $name/trimmed_results/"$name"_R2.trimmed.fastq.gz > $name/Results/"$name".sam
-
-# use samtools to convert sam files to bam
-samtools view -S -b $name/Results/"$name".sam > $name/Results/"$name".bam
-
-# perform sorting of bam files
-samtools sort -o $name/Results/"$name".sorted.bam $name/Results/"$name".bam
-
-# perform indexing of bam files
-
-samtools index $name/Results/"$name".sorted.bam
-
-# use bcftools to pileup format 
-bcftools mpileup -O b -o $name/Results/"$name".bcf -f $name/data/reference.fasta $name/Results/"$name".sorted.bam
-
-# Call variants using bcftool and create vcf file
-bcftools call -m -v -o $name/Results/"$name".variants.vcf $name/Results/"$name".bcf
-
-echo "successfully done!"
 
 
 
