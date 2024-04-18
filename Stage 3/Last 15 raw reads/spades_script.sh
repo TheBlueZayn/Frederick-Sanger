49636591a4aa56c523141352739353da2bfd7894
Mariamkhaled15 <163095154+Mariamkhaled15@users.noreply.github.com>
2024-04-18 06:26:27 +0200

    Add files via upload

---
+++ Stage 3/Last 15 raw reads/spades_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+spades.py --careful -1 SRR8159842_Whole_genome_Illumina_MiSeq_sequence_of_Salmonella_enterica_subsp._enterica_serovar_Concord_1.trim.fastq -2 SRR8159842_Whole_genome_Illumina_MiSeq_sequence_of_Salmonella_enterica_subsp._enterica_serovar_Concord_2.trim.fastq -o genome.assembly/SRR8159842
+spades.py --careful -1 SRR8159839_Whole_genome_Illumina_MiSeq_sequence_of_Salmonella_enterica_subsp._enterica_serovar_Concord_1.trim.fastq -2 SRR8159839_Whole_genome_Illumina_MiSeq_sequence_of_Salmonella_enterica_subsp._enterica_serovar_Concord_2.trim.fastq -o genome.assembly/SRR8159839
+spades.py --careful -1 SRR7514404_Other_Sequencing_of_Salmonella_enterica_1.trim.fastq -2 SRR7514404_Other_Sequencing_of_Salmonella_enterica_2.trim.fastq -o genome.assembly/SRR7514404
+spades.py --careful -1 ERR9516356_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516356_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516356
+spades.py --careful -1 ERR9516354_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516354_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516354
+spades.py --careful -1 ERR9516349_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516349_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516349
+spades.py --careful -1 ERR9516338_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516338_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516338
+spades.py --careful -1 ERR9516287_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516287_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516287
+spades.py --careful -1 ERR9516228_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516228_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516228
+spades.py --careful -1 ERR9516226_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516226_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516226
+spades.py --careful -1 ERR9516191_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516191_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516191
+spades.py --careful -1 ERR9516189_HiSeq_X_Ten_paired_end_sequencing_1.trim.fastq -2 ERR9516189_HiSeq_X_Ten_paired_end_sequencing_2.trim.fastq -o genome.assembly/ERR9516189
+spades.py --careful -1 ERR3635344_Illumina_NovaSeq_6000_paired_end_sequencing_10k_Salmonella_Project_1.trim.fastq -2 ERR3635344_Illumina_NovaSeq_6000_paired_end_sequencing_10k_Salmonella_Project_2.trim.fastq -o genome.assembly/ERR3635344
