0c2386d1de0cb5590f55b0661f99e604c46e3a60
Mariamkhaled15 <163095154+Mariamkhaled15@users.noreply.github.com>
2024-04-18 06:23:02 +0200

    Add files via upload

---
+++ Stage 3/Last 15 raw reads/amr_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Loop through all folders in the current directory
+for folder in */; do
+    # Extract sample name (adjust based on your naming convention)
+    sample_name="${folder%/}"
+
+  amrfinder -n ${sample_name}/contigs.fasta -O Salmonella -o AMRoutput/${sample_name}.csv
+done
+
+rm AMRoutput/plasmidfinder_output.csv
+rm AMRoutput/SeqSero2results.csv
+rm AMRoutput/AMRoutput.csv
