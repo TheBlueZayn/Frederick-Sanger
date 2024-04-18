ab6cfc2b1a2b5d1c15e312070fbe12542d11c3cd
MUHAMADFAIZANKHAN <162900134+MUHAMADFAIZANKHAN@users.noreply.github.com>
2024-04-18 16:39:57 +0500

    Add files via upload

---
+++ Stage 3/intial 20 raw reads/seqsero2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Define an array of samples
+samples=("ERR9516208" "ERR9516260" "ERR9516213" "ERR9516215" "ERR9516190" "ERR9516264" "ERR9516186" "ERR9516341" "ERR9516233" "ERR9516202" "ERR9516219" "ERR9516192" "ERR9516194" "SRR13221299" "ERR9516319" "ERR9516292" "ERR9516266" "SRR11927768" "ERR9516298" "SRR11927771")
+
+# Loop over each sample
+for sample in "${samples[@]}"; do
+    # Run SeqSero2 using the output of a SPAdes assembly
+    SeqSero2_package.py -m k -t 4 -i "spadesdata/${sample}_spades/contigs.fasta" -d "serotyping/${sample}"
+done
