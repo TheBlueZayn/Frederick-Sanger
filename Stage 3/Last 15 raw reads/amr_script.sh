8a2113a36da8ca13828f9efb156d1d3c7001f375
Mariamkhaled15 <163095154+Mariamkhaled15@users.noreply.github.com>
2024-04-19 21:29:31 +0200

    Update amr_script.sh

--- Stage 3/Last 15 raw reads/amr_script.sh
+++ Stage 3/Last 15 raw reads/amr_script.sh
@@ -5,7 +5,7 @@ for folder in */; do
     # Extract sample name (adjust based on your naming convention)
     sample_name="${folder%/}"
 
-  amrfinder -n ${sample_name}/contigs.fasta -O Salmonella -o AMRoutput/${sample_name}.csv
+  amrfinder -n ${sample_name}/contigs.fasta -O Salmonella --name ${sample_name} -o AMRoutput/${sample_name}.csv
 done
 
 rm AMRoutput/plasmidfinder_output.csv
